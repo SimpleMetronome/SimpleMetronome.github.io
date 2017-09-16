80a26b7f01ea87fde60a720b5d1c4f3756653b14
Jonathan Herman <jdh11235@gmail.com>
2017-09-15 21:30:40 -0500

    draw dial

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
-var metronomeContainer = d3.select('#metronome')
+// this was helpful in understanding how to draw clock-like objects http://bl.ocks.org/tomgp/6475678
 
-var containerSize
-function resizeContainer() {
-  containerSize = Math.min(window.innerWidth, window.innerHeight)
-  metronomeContainer.attr('width', containerSize)
+// page setup
+
+// TODO resize stuff page size changes, for now manually refresh
+
+var metronome = d3.select('body').append('svg')
+  .attr('id', 'metronome')
+
+var containerSize = Math.min(window.innerWidth, window.innerHeight)
+metronome.attr('width', containerSize)
                     .attr('height', containerSize)
                     .style('top', (window.innerHeight - containerSize) / 2 )
                     .style('left', (window.innerWidth - containerSize) / 2 )
-}
-resizeContainer()
-window.addEventListener('resize', resizeContainer)
 
 window.addEventListener('touchmove', function(event) {
   event.preventDefault()
 })
+
+// draw metronome
+
+// TODO remember with localstorage
+var bpm = 120
+
+function tickScale() {
+  return d3.scaleLinear()
+    .range([0, 360 - (360 / bpm)])
+    .domain([0, bpm - 1])
+}
+
+function dialRadius() {
+  return containerSize / 2
+}
+
+var face = metronome.append('g')
+  .attr('id', 'face')
+  .attr('transform', `translate(${dialRadius()}, ${dialRadius()})`)
+
+face.selectAll('.tick')
+  .data(d3.range(0, bpm)).enter()
+    .append('line')
+    .attr('class', 'tick')
+    .attr('x1', 0)
+    .attr('x2', 0)
+    .attr('y1', dialRadius() * 0.95)
+    .attr('y2', dialRadius() * 0.9)
+    .attr('transform', function(d) {
+      return `rotate(${tickScale()(d)})`
+    })
